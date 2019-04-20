61dfa187aa89961b4d9926cad929b7185c1d1e82
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-04-20 16:56:19 +0200

    Add lab 2

---
+++ lab_2/blog/app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+    #   http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
+    before_action :authenticate_user!
+    
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = current_user.article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+        authorize! :manage, @article
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        authorize! :manage, @article
+        if @article.update(article_params)
+        redirect_to @article
+        else
+        render 'edit'
+        end
+    end
+
+    def create
+        @article = current_user.article.new(article_params)
+
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+   
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        authorize! :manage, @article
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+   
+end
